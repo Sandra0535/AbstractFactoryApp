c582b83243506f57d1c1f64f2eecfd7733b8cf75
Aurionpro <Aurionpro@ACER1208>
2023-09-22 20:00:43 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using AbstractFactoryLibrary.Model;
+
+namespace AbstractFactoryApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IAutoFactory teslaSUVFactory = new SUVFactory();
+            ITesla teslaSUV = teslaSUVFactory.CreateTesla();
+            teslaSUV.Start();
+            teslaSUV.Stop();
+            Console.WriteLine();
+            IAutoFactory teslaSedanFactory = new SedanFactory();
+            ITesla teslaSedan = teslaSedanFactory.CreateTesla();
+            teslaSedan.Start();
+            teslaSedan.Stop();
+        }
+    }
+}
